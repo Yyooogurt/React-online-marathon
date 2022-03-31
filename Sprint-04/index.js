ea3407598d67f85e261d4d51151fd20ae1088b5c
Egor <example.@icloud.com>
2022-03-31 18:20:05 +0300

    Spint 5 added

--- Sprint-04/index.js
+++ Sprint-04/index.js
@@ -18,9 +18,8 @@
 //Task 2
 // function add(x, y) {
 //     return new Promise((resolve, reject) => {
-//       if (typeof x == "number" && typeof y == "number") {
+//       if (typeof x === "number" && typeof y === "number") {
 //         resolve(x + y);
-//         return;
 //       }
 //       reject("Error!");
 //     });
---
+++ Sprint-05/Task 0/index.js
@@ -0,0 +1,13 @@
+//Task 1
+//const localStorage = require("./localStorage.js");
+// localStorage.setItem("login", "Tom12!");
+// localStorage.setItem("token", "QhuR56Rw");
+
+//Task 2
+//localStorage.setItem("user",JSON.stringify({ name: "Tom", age: 25 }));
+
+//Task 3
+//localStorage.removeItem("name");
+
+//Task 4
+//localStorage.clear();
